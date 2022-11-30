45883cd34b76ae4e20db24c4780485fb425af67c
BrenoNAlmeida <brenonascialmeida@gmail.com>
2022-11-30 09:43:40 -0300

    frontend - estrutura inicial do projeto

--- backend/django_react/settings.py
+++ backend/django_react/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
 
-'corsheaders.middleware.CorsMiddlware',   
+'corsheaders.middleware.CorsMiddleware',   
  'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- backend/django_react/urls.py
+++ backend/django_react/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include,re_path
-drom django.views.genereic import templateView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
