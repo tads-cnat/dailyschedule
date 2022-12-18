0603cb68f367e4f8c60b6dc2effa776be71d1c76
Jefferson Ribeiro <ribeiro.jefferson@escolar.ifrn.edu.br>
2022-12-18 11:40:13 -0300

    Adiciona campo de busca na API

--- backend/core/dailyschedule/views.py
+++ backend/core/dailyschedule/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 
 from .models import Cronograma, Tarefa, Aluno
 from .serializers import SerializadorCronograma, SerializadorTarefa, SerializadorAluno
@@ -12,6 +12,8 @@ from rest_framework.response import Response
 class CronogramaViewSet(viewsets.ModelViewSet):
     queryset = Cronograma.objects.all()
     serializer_class = SerializadorCronograma
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['titulo']
 
     @action(detail=True, methods=['get'], url_path='tarefas')
     def get_tarefas(self, request, pk=None):
