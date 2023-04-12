1862b3b75e38ccd1fb226d757dd91e3a562181ff
Christian Bruno <christianbrunorn@hotmail.com>
2023-04-12 10:09:23 -0300

    Removendo Importação Não Utilizada
    Remoção da importação da classe Users para ciração de usuários no django, pois utilizamos o AbstractUser. Correção feita utilizando a ferramenta Pylint para encontrar Code Smells, trecho reportador pela ferramenta: models.py:3:0: W0611: Unused User imported from django.contrib.auth.models (unused-import)

--- backend/core/dailyschedule/models.py
+++ backend/core/dailyschedule/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 import datetime
-
-from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
 
