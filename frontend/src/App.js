733182bed31607440030ffe49c7bcdcf2f2cbd81
Jefferson Ribeiro <ribeiro.jefferson@escolar.ifrn.edu.br>
2023-05-09 19:05:15 -0300

    Adiciona  .catch para lidar com a rejection na promise

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,8 +31,12 @@ function App() {
 		const alerta = () => {
 			fetch(`http://localhost:8000/api/aluno/${id}/alerta`)
 				.then((response) => response.json())
-				.then((data) => setAlerta(data));
+				.then((data) => setAlerta(data))
+				.catch((err) => {
+					console.error(err);
+				});
 		};
+		
 		setInterval(alerta(), 2400000);
 	}, []);
 
