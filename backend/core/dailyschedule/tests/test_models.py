30078bafc4114169c79e2917119700790bf1ceae
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-05-24 04:57:15 +0000

    Adiciona os testes unitários do método equals

---
+++ backend/core/dailyschedule/tests/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from models import Aluno
+
+class AlunoTestCase(TestCase):
+    
+    def setUp(self):
+        self.aluno1 = Aluno(first_name='João', last_name='Silva', email='joao@example.com', username='joao123', password='senha123')
+        self.aluno2 = Aluno(first_name='João', last_name='Silva', email='joao@example.com', username='joao123', password='senha123')
+        self.aluno3 = Aluno(first_name='Maria', last_name='Santos', email='maria@example.com', username='maria456', password='senha456')
+
+    def test_eq_same_objects(self):
+        self.assertEqual(self.aluno1, self.aluno1)
+
+    def test_eq_equal_objects(self):
+        self.assertEqual(self.aluno1, self.aluno2)
+
+    def test_eq_different_objects(self):
+        self.assertNotEqual(self.aluno1, self.aluno3)
