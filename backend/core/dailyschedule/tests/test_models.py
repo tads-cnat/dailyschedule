7cf4431354b5eaa44016d570d0a0b5b68a7bbc03
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-05-24 05:08:41 +0000

    Adiciona os testes unitários do natural comparator

--- backend/core/dailyschedule/tests/test_models.py
+++ backend/core/dailyschedule/tests/test_models.py
@@ -8,11 +8,17 @@ class AlunoTestCase(TestCase):
         self.aluno2 = Aluno(first_name='João', last_name='Silva', email='joao@example.com', username='joao123', password='senha123')
         self.aluno3 = Aluno(first_name='Maria', last_name='Santos', email='maria@example.com', username='maria456', password='senha456')
 
-    def test_eq_same_objects(self):
+    def test_aluno_eq_same_objects(self):
         self.assertEqual(self.aluno1, self.aluno1)
 
-    def test_eq_equal_objects(self):
+    def test_aluno_eq_equal_objects(self):
         self.assertEqual(self.aluno1, self.aluno2)
 
-    def test_eq_different_objects(self):
+    def test_aluno_eq_different_objects(self):
         self.assertNotEqual(self.aluno1, self.aluno3)
+
+    def test_aluno_ordering(self):
+        self.assertLess(self.aluno2, self.aluno1)
+        self.assertLessEqual(self.aluno1, self.aluno3)
+        self.assertGreater(self.aluno1, self.aluno2)
+        self.assertGreaterEqual(self.aluno1, self.aluno1)
