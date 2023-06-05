87d31fab4044fb62cab351204a14476d71098884
Deivdson Pereira Oliveira da Silva <20211014040037@ifrn.local>
2023-06-05 13:55:25 -0300

    correções no componente Form

--- frontend/src/components/Forms/InputFormCrono.js
+++ frontend/src/components/Forms/InputFormCrono.js
@@ -1,13 +1,29 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-import '../style.css'
+import PropTypes from 'prop-types';
 
-const InputCriarCrono = ({ type, name, id, onChange, value }) => {
-  return (
-    <div>
-      <input type={type} name={name} id={id} onChange={onChange} value={value} /> <br/>
-  </div>
-  )
-}
+const InputCriarCrono = ({type, name, id, onChange, value}) => {
+	return (
+		<div>
+			<input
+				type={type}
+				name={name}
+				id={id}
+				onChange={onChange}
+				value={value}
+			/>{' '}
+			<br />
+		</div>
+	);
+};
+
+InputCriarCrono.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
 
 export default InputCriarCrono;
