2e9ae448032aaba3569f4a77ead5e016f81e9398
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-07-04 17:41:04 +0000

    Adiciona testes de sistema: CRUD, CadastroView e AuthView

--- backend/core/dailyschedule/tests.py
+++ backend/core/dailyschedule/tests.py
@@ -4,6 +4,10 @@ from django.urls import reverse, resolve
 from rest_framework import status
 import json
 from .models import Aluno, Cronograma
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
 User = get_user_model()
 
@@ -66,7 +70,7 @@ class AlunoCRUDIntegrationTests(TestCase):
         Aluno.objects.filter(id=aluno.id).delete()
         self.assertFalse(Aluno.objects.filter(id=aluno.id).exists())
 
-class AlunoCRUDSystemTests(TestCase):
+class AlunoCRUDSystemTest(TestCase):
     def setUp(self):
         self.username = 'testuser'
         self.password = 'testpassword'
@@ -104,93 +108,76 @@ class AlunoCRUDSystemTests(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_update_aluno(self):
-        user = User.objects.create_user(username='example_user', password='example_password', email='existing_email@example.com')
-        updated_data = {
-            'first_name': 'Jane',
-            'last_name': 'Smith',
-            'email': 'jane.smith@example.com',
-            'username': 'example_user',
-            'password': 'example_password',
-        }
-        url = reverse('Aluno-detail', kwargs={'pk': user.id})
-        response = self.client.put(url, data=json.dumps(updated_data), content_type='application/json')
-        
-        # Print para debug
-        print(response.content)
-        # Print para debug
-        print(response.data)
-        
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
-
-
-    def test_delete_aluno(self):
-        user = User.objects.create_user(**self.aluno_data)
-        url = reverse('Aluno-detail', kwargs={'pk': user.id})
-        response = self.client.delete(url)
-
-        # Print para debug
-        print(response.content)
-        # Print para debug
-        print(response.data)
-
-
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(User.objects.filter(username=self.username).exists())
-
-class CronogramaIntegrationTests(TestCase):
+class CadastroAlunoSystemTest(TestCase):
     def setUp(self):
-        self.aluno = {
-            'first_name': 'Lucas',
-            'last_name': 'Melo',
-            'email': 'lucas@mail.com',
-            'username': 'testuser',
-            'password': 'testpassword',
-            'notificacao': False,
-            'qtd': 0
-        }
-        self.cronograma = {
-            'privacidade': False,
-            'titulo': "Cronograma Teste",
-            'aluno': self.aluno
+        self.client = APIClient()
+
+    def test_cadastro_aluno(self):
+        # Dados de exemplo para o cadastro
+        data = {
+            'usuario': 'jefferson',
+            'senha': 'password123',
+            'confirmar_senha': 'password123',
+            'email': 'jefferson@mail.com',
+            'primeiro_nome': 'Jefferson',
+            'ultimo_nome': 'R',
         }
 
-    def test_criar_cronograma(self):
-        cronograma = Cronograma.objects.create(**self.cronograma)
-        self.assertEqual(cronograma.privacidade, self.cronograma['privacidade'])
-        self.assertEqual(cronograma.titulo, self.cronograma['titulo'])
-        self.assertEqual(cronograma.aluno, self.cronograma['aluno'])
+        # Faz uma solicitação POST para a URL 'alunos'
+        response = self.client.post('/alunos/', data, format='json')
 
-    def test_atualizar_cronograma(self):
-        cronograma = Cronograma.objects.create(**self.cronograma)
-        updated_data = {
-            'titulo': 'Titulo teste',
-            'privacidade': True
+        # Verifica o código de status da resposta
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Verifica se o aluno foi criado no banco de dados
+        self.assertTrue(Aluno.objects.filter(username='john_doe').exists())
+
+class AuthSystemTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login(self):
+        # Dados de exemplo para o login
+        data = {
+            'usuario': 'jefferson',
+            'senha': 'password123',
         }
-        Cronograma.objects.filter(id=cronograma.id).update(**updated_data)
-        updated_cronograma = Cronograma.objects.get(id=cronograma.id)
-        self.assertEqual(updated_cronograma.privacidade, updated_data['privacidade'])
-        self.assertEqual(updated_cronograma.titulo, updated_data['titulo'])
 
-    def test_deletar_cronograma(self):
-        cronograma = Cronograma.objects.create(**self.cronograma)
-        Cronograma.objects.filter(id=cronograma.id).delete()
-        self.assertFalse(Cronograma.objects.filter(id=cronograma.id).exists())
-    
-    def test_obter_cronograma_por_titulo(self):
-        cronograma = Cronograma.objects.create(**self.cronograma)
-        cronograma_obtido = Cronograma.objects.get(titulo=cronograma.titulo)
-        self.assertEqual(cronograma_obtido.titulo, cronograma['titulo'])
-    
-    def test_obter_cronogramas_por_aluno(self):
-        cronograma1 = Cronograma.objects.create(**self.cronograma)
-        cronograma2 = Cronograma.objects.create(
-            privacidade=False,
-            titulo="Cronograma 2"
-        )
-        cronogramas_aluno = Cronograma.objects.filter(aluno=self.aluno)
-        self.assertEqual(cronogramas_aluno.privacidade, cronograma1['privacidade'])
-        self.assertEqual(cronogramas_aluno.titulo, cronograma1['titulo'])
-        self.assertEqual(cronogramas_aluno.aluno, cronograma1['aluno'])
-        self.assertIn(cronograma1, cronogramas_aluno)
-        self.assertNotIn(cronograma2, cronogramas_aluno)
+        # Faz uma solicitação POST para a URL 'auth/login'
+        response = self.client.post('/auth/login/', data, format='json')
+
+        # Verifica o código de status da resposta
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Verifica se o login foi bem-sucedido
+        self.assertEqual(response.data['detail'], 'login realizado com sucesso')
+
+    def test_cadastro(self):
+        # Dados de exemplo para o cadastro
+        data = {
+            'usuario': 'jefferson',
+            'senha': 'password123',
+            'confirmar_senha': 'password123',
+            'email': 'jefferson@mail.com',
+            'primeiro_nome': 'Jefferson',
+            'ultimo_nome': 'R',
+        }
+
+        # Faz uma solicitação POST para a URL 'auth/cadastro'
+        response = self.client.post('/auth/cadastro/', data, format='json')
+
+        # Verifica o código de status da resposta
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Verifica se o usuário foi cadastrado com sucesso
+        self.assertEqual(response.data['message'], 'Usuário cadastrado com sucesso')
+
+    def test_logout(self):
+        # Faz uma solicitação GET para a URL 'auth/logout'
+        response = self.client.get('/auth/logout/')
+
+        # Verifica o código de status da resposta
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Verifica se o logout foi realizado com sucesso
+        self.assertEqual(response.data['message'], 'Logout realizado com sucesso')
